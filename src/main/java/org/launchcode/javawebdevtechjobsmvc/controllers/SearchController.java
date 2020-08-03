a55624e5a07d5cc7e35bffafbdb64d7150ccf98b
aniorill <x.iorillo@gmail.com>
2020-08-02 22:32:53 -0400

    Created search bar display list

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -26,19 +26,19 @@ public class SearchController {
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
     @PostMapping(value="results")
-    public String displaySearchResults(Model model, @RequestParam String searchTerm, @RequestParam String searchType){
+    public String displaySearchResults(@RequestParam String searchTerm, @RequestParam String searchType, Model model){
         ArrayList<Job> jobs;
 
-        if (searchTerm.equals("all") || searchTerm.equals("")){
-           jobs = JobData.findAll();
-        } else {
+        //if (searchTerm.equals("all") || searchTerm.equals("")){
+           //jobs = JobData.findAll();
+        //} else {
             jobs = JobData.findByColumnAndValue(searchType, searchTerm);
-        }
+        //}
 
         model.addAttribute("jobs", jobs);
         model.addAttribute("columns", columnChoices);
 
-        return "search/results";
+        return "search";
 
     }
 }
